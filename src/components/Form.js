952af7e645ce89bdfb0e35993c5a7f6cf094889b
Denis <d.combe@laposte.net>
2022-02-15 18:10:20 +0100

    ajout du Card et insertion des images

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Card from './Card';
 
 const Form = () => {
    const [moviesData, setMoviesData] = useState([]);
@@ -24,7 +25,11 @@ const Form = () => {
                <div className="btn-sort" id="badToGood">Flop<span>&#8594;</span></div>
             </div>
          </div>
-         <div className="result"></div>
+         <div className="result">
+            {moviesData.slice(0, 12).map((movie) => (
+               <Card key={movie.id} movie={movie} />
+            ))}
+         </div>
       </div>
    );
 };

db0967e9bd548fbf8c718ea4bf5a55bb87f27f0d
Denis <d.combe@laposte.net>
2022-02-15 16:55:12 +0100

    ajout api de TMDB - fetch avec AXIOS - création de setMoviesDatas <= useState

---
+++ src/components/Form.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const Form = () => {
+   const [moviesData, setMoviesData] = useState([]);
+
+
+   useEffect(() => {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=5b40bab157cf097dda63cb65316f627e&query=star&language=fr-FR')
+         .then((res) => { setMoviesData(res.data.results); })
+   }, [])
+
+   return (
+      <div className="form-component">
+         <div className="form-container">
+            <form>
+               <input type="text" placeholder="Entrez le titre d'un film" id="search-input" />
+               <input type="submit" value="Rechercher" />
+            </form>
+            <div className="btn-sort-container">
+               <div className="btn-sort" id="goodToBad">Top<span>&#8594;</span></div>
+            </div>
+            <div className="btn-sort-container">
+               <div className="btn-sort" id="badToGood">Flop<span>&#8594;</span></div>
+            </div>
+         </div>
+         <div className="result"></div>
+      </div>
+   );
+};
+
+export default Form;

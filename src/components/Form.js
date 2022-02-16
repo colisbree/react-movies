1a3df94bd151c686bf51b62708d59cfd233e8bd6
Denis <d.combe@laposte.net>
2022-02-16 10:51:07 +0100

    champ recherche OK - callback sur la fonction useEffect

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,22 @@ import Card from './Card';
 
 const Form = () => {
    const [moviesData, setMoviesData] = useState([]);
-
+   const [search, setSearch] = useState("code"); // code c'est juste pour l'exemple
 
    useEffect(() => {
-      axios.get('https://api.themoviedb.org/3/search/movie?api_key=5b40bab157cf097dda63cb65316f627e&query=avenger&language=fr-FR')
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=5b40bab157cf097dda63cb65316f627e&query=${search}&language=fr-FR`)
          .then((res) => { setMoviesData(res.data.results); })
-   }, [])
+   }, [search])
 
    return (
       <div className="form-component">
          <div className="form-container">
             <form>
-               <input type="text" placeholder="Entrez le titre d'un film" id="search-input" />
+               <input type="text"
+                  placeholder="Entrez le titre d'un film"
+                  id="search-input"
+                  onChange={(e) => setSearch(e.target.value)}
+               />
                <input type="submit" value="Rechercher" />
             </form>
             <div className="btn-sort-container">

ea39f86379d83d1f52e5c7f0aea600e645762a29
Denis <d.combe@laposte.net>
2022-02-16 15:22:09 +0100

    tri croissant décroissant OK

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,7 @@ import Card from './Card';
 const Form = () => {
    const [moviesData, setMoviesData] = useState([]);
    const [search, setSearch] = useState("code"); // code c'est juste pour l'exemple
+   const [sortGoodBad, setSortGoodBad] = useState(null); // de base pas de tri
 
    useEffect(() => {
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=5b40bab157cf097dda63cb65316f627e&query=${search}&language=fr-FR`)
@@ -23,18 +24,39 @@ const Form = () => {
                <input type="submit" value="Rechercher" />
             </form>
             <div className="btn-sort-container">
-               <div className="btn-sort" id="goodToBad">Top<span>&#8594;</span></div>
+               <div
+                  className="btn-sort"
+                  id="goodToBad"
+                  onClick={() => setSortGoodBad("goodToBad")}
+               >
+                  Top<span>&#8594;</span>
+               </div>
             </div>
             <div className="btn-sort-container">
-               <div className="btn-sort" id="badToGood">Flop<span>&#8594;</span></div>
+               <div
+                  className="btn-sort"
+                  id="badToGood"
+                  onClick={() => setSortGoodBad("badToGood")}
+               >
+                  Flop <span span >&#8594;</span>
+               </div>
             </div>
-         </div>
+         </div >
          <div className="result">
-            {moviesData.slice(0, 12).map((movie) => (
-               <Card key={movie.id} movie={movie} />
-            ))}
+            {moviesData
+               .slice(0, 12)
+               .sort((a, b) => {
+                  if (sortGoodBad === "goodToBad") {
+                     return b.vote_average - a.vote_average;
+                  } else if (sortGoodBad === "badToGood") {
+                     return a.vote_average - b.vote_average;
+                  }
+               })
+               .map((movie) => (
+                  <Card key={movie.id} movie={movie} />
+               ))}
          </div>
-      </div>
+      </div >
    );
 };
 
